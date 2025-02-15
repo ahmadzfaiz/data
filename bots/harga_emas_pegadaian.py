e6958e7ad43f54a10136e5ad7dfd331049d8e75a
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2025-02-15 14:09:28 +0700

    optimize Harga Emas Pegadaian scraper

--- bots/harga_emas_pegadaian.py
+++ bots/harga_emas_pegadaian.py
@@ -3,8 +3,6 @@ import datetime
 import re
 import uuid
 import logging
-from pathlib import Path
-from tempfile import gettempdir
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -34,49 +32,49 @@ title = driver.title
 logging.info(f"Page: {title}")
 
 try:
-    beli_emas_xpath = '//*[@id="__nuxt"]/div/div[2]/main/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/div/div/div[1]'
-    beli_emas_element = wait.until(EC.presence_of_element_located((By.XPATH, beli_emas_xpath)))
-    beli_emas_text = beli_emas_element.text
+  beli_emas_xpath = '//*[@id="__nuxt"]/div/div[2]/main/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/div/div/div[1]'
+  beli_emas_element = wait.until(EC.presence_of_element_located((By.XPATH, beli_emas_xpath)))
+  beli_emas_text = beli_emas_element.text
 except Exception as e:
-    logging.error(f"Harga Beli: {str(e)}")
-    beli_emas_text = None
+  logging.error(f"Harga Beli: {str(e)}")
+  beli_emas_text = None
 
 try:
-    jual_emas_xpath = '//*[@id="__nuxt"]/div/div[2]/main/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/div/div/div[2]'
-    jual_emas_element = wait.until(EC.presence_of_element_located((By.XPATH, jual_emas_xpath)))
-    jual_emas_text = jual_emas_element.text
+  jual_emas_xpath = '//*[@id="__nuxt"]/div/div[2]/main/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div[2]/div/div/div[2]'
+  jual_emas_element = wait.until(EC.presence_of_element_located((By.XPATH, jual_emas_xpath)))
+  jual_emas_text = jual_emas_element.text
 except Exception as e:
-    logging.error(f"Harga Jual: {str(e)}")
-    jual_emas_text = None
+  logging.error(f"Harga Jual: {str(e)}")
+  jual_emas_text = None
 
 def get_harga(text):
-    re_match = re.search(r"Rp\s*([\d\.]+)", text)
-    if re_match:
-        return re_match.group(1).replace(".", "")
+  re_match = re.search(r"Rp\s*([\d\.]+)", text)
+  if re_match:
+    return re_match.group(1).replace(".", "")
 
 # Save data into CSV file
 if beli_emas_text and jual_emas_text:
-    new_data = {
-        "id": str(uuid.uuid4()),
-        "harga_beli": get_harga(beli_emas_text), 
-        "harga_jual": get_harga(jual_emas_text), 
-        "timestamp": datetime.datetime.now()
-    }
+  new_data = {
+    "id": str(uuid.uuid4()),
+    "harga_beli": get_harga(beli_emas_text), 
+    "harga_jual": get_harga(jual_emas_text), 
+    "timestamp": datetime.datetime.now()
+  }
 
-    output_file = Path(gettempdir()) / "harga_emas_pegadaian.csv"
-    
-    with open(output_file, "a", newline="", encoding="utf-8") as f:
-        field_names = ["id", "harga_beli", "harga_jual", "timestamp"]
-        writer = csv.DictWriter(f, fieldnames=field_names)
+  output_file = "datasets/harga_emas_pegadaian.csv"
+  
+  with open(output_file, "a", newline="", encoding="utf-8") as f:
+    field_names = ["id", "harga_beli", "harga_jual", "timestamp"]
+    writer = csv.DictWriter(f, fieldnames=field_names)
 
-        if f.tell() == 0:
-            writer.writeheader()
+    if f.tell() == 0:
+      writer.writeheader()
 
-        writer.writerow(new_data)
+    writer.writerow(new_data)
 
-    logging.info("New row of harga emas added successfully!")
+  logging.info("New row of harga emas added successfully!")
 
 else:
-    logging.warning("Pegadaian Harga page failed to scrape")
+  logging.warning("Pegadaian Harga page failed to scrape")
 
 driver.quit()
